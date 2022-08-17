cf5f2edf08f436a07d7772e9663144cb1271b719
Imarino4 <ignacio.martacos@gmail.com>
2022-08-17 07:00:07 -0700

    Betty fixed on _pall.c

--- _pall.c
+++ _pall.c
@@ -2,9 +2,10 @@
 
 void _pall(stack_t **head_ref, unsigned int line_number)
 {
+	stack_t *aux;
+       	
 	(void)line_number;
-	stack_t *aux = *head_ref;
-
+	aux = *head_ref;
 	while (aux)
 	{
 		printf("%d\n", aux->n);
