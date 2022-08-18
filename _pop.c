2092442668003ab5c181ba84721ad68729e6e497
Denisse Landau <denisselandau97@gmail.com>
2022-08-18 08:11:27 -0700

    valgrind fix in _pop

--- _pop.c
+++ _pop.c
@@ -1,13 +1,13 @@
 #include "monty.h"
-
 /**
  * _pop - removes the top element of the stack
  * @head_ref: pointer to first element of a stack
  * @line_numbers: number of line
  */
-
 void _pop(stack_t **head_ref, unsigned int line_numbers)
 {
+	stack_t *aux = (*head_ref);
+
 	if (*head_ref)
 		*head_ref = (*head_ref)->next; /*Move head to second element*/
 	else
@@ -15,6 +15,7 @@ void _pop(stack_t **head_ref, unsigned int line_numbers)
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_numbers);
 		exit(EXIT_FAILURE);
 	}
-	if (*head_ref) /*if after moving still exist*/
-		(*head_ref)->prev = NULL;
+	/*if (*head_ref) if after moving still exist*/
+		/*(*head_ref)->prev = NULL;*/
+	free(aux); /* frees the space that was popped */
 }
